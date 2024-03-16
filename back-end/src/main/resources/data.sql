3656c6e1febbbb05c7dcc14da1f21f6dc0686cd1
vikram.bindal <vikram.bindal@entelect.co.za>
2024-03-16 22:14:24 +0200

    default roles

---
+++ back-end/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT IGNORE INTO `role`(id, date_created, date_updated, version, name)
+VALUES('430e083c-e3cf-11ee-b074-0242ac110002', now(), now(), 0, 'ADMIN');
+INSERT IGNORE INTO `role`(id, date_created, date_updated, version, name)
+VALUES('430fa4ba-e3cf-11ee-b074-0242ac110002', now(), now(), 0, 'USER');
+INSERT IGNORE INTO `role`(id, date_created, date_updated, version, name)
+VALUES('431161fd-e3cf-11ee-b074-0242ac110002', now(), now(), 0, 'MANAGER');
